89e70844807cd6c7641fe6ae87751ed9c146f956
haiderzaman1781 <haiderzaman1781@gmail.com>
2024-12-01 12:17:00 +0500

    ok

--- javascript/main.js
+++ javascript/main.js
@@ -20,27 +20,54 @@ navLinks.forEach(link => {
   });
 });
 
-
 window.addEventListener("load", () => {
 
   const navbar = document.getElementById("main-nav");
+  const navLinks = document.getElementById("nav-Links");
+  const navLogo = document.getElementById("nav-logo");
 
   // Add the initial transition styles
-  navbar.style.transition = "all 0.3s ease"; // Transition for width, background-color, and backdrop-filter
+  navbar.style.transition = "all 0.3s ease";
+  navLinks.style.transition = "all .5s ease";
 
-  window.addEventListener("scroll", () => {
+  // Function to check if the screen size is large
+  const isLargeScreen = () => window.innerWidth >= 1024;
+
+  // Function to handle scroll animation logic
+  const handleScrollAnimation = () => {
     const scrollY = window.scrollY;
 
     if (scrollY > 650) {
       navbar.style.width = "100%";
-      navbar.style.background = "linear-gradient(to bottom, #6b46c1, #3b82f6);";
+      navbar.style.background = "linear-gradient(to right, #6b46c1, #3b82f6)";
       navbar.style.backdropFilter = "blur(10px)";
+      navLinks.style.backgroundColor = "transparent";
+      navLogo.style.backgroundColor = "transparent";
+      navbar.style.boxShadow = "0px 10px 10px -5px rgba(255, 255, 255, 0.5)";
     } else {
       navbar.style.width = "78%";
       navbar.style.background = "transparent";
       navbar.style.backdropFilter = "none";
+      navLinks.style.backgroundColor = "gray";
+      navLogo.style.backgroundColor = "black";
+      navbar.style.boxShadow = "0px 0px 0px 0px rgba(0, 0, 0, 0)";
+
+    }
+  };
+
+  if (isLargeScreen()) {
+    window.addEventListener("scroll", handleScrollAnimation);
+  }
+  if (!isLargeScreen()) {
+    navbar.style.background = "linear-gradient(to left, #6b46c1, #3b82f6)"
+  }
+
+  window.addEventListener("resize", () => {
+    if (isLargeScreen()) {
+      window.addEventListener("scroll", handleScrollAnimation);
+    } else {
+      window.removeEventListener("scroll", handleScrollAnimation);
     }
   });
-
 });
 
